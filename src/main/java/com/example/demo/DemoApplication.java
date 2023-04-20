30245efda41e02a2be58c67e4cb357130ec50982
Anurag Sharma (anushar4) <anushar4@cisco.com>
2023-04-20 10:39:21 +0530

    master01

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String fetchData() throws Exception {
+		return "OpenShift-SpringBoot is Working";
+	}
+}
